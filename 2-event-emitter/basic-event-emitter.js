c2c2144470c8c34413089f8dc55b7ddad4251d7e
Andres Bedoya <mortiis.angel@gmail.com>
2024-07-12 10:48:22 -0500

    Basic event emitter using functions

--- 2-event-emitter/basic-event-emitter.js
+++ 2-event-emitter/basic-event-emitter.js
@@ -13,19 +13,50 @@ class SimpleEventEmitter {
 
   emit(event, ...args) {
     if (this.listeners[event]) {
-      this.listeners[event].forEach(callback => callback(...args));
+      this.listeners[event].forEach((callback) => callback(...args));
     }
   }
 
   off(event, callback) {
     if (this.listeners[event]) {
-      this.listeners[event] = this.listeners[event].filter(cb => cb !== callback);
+      this.listeners[event] = this.listeners[event].filter(
+        (cb) => cb !== callback,
+      );
     }
   }
 }
 
+function createSimpleEventEmitter() {
+  const listeners = {};
+
+  function on(event, callback) {
+    if (!listeners[event]) {
+      listeners[event] = [];
+    }
+    listeners[event].push(callback);
+  }
+
+  function emit(event, ...args) {
+    if (listeners[event]) {
+      listeners[event].forEach((callback) => callback(...args));
+    }
+  }
+
+  function off(event, callback) {
+    if (listeners[event]) {
+      if (callback) {
+        listeners[event] = listeners[event].filter((cb) => cb !== callback);
+      } else {
+        delete listeners[event];
+      }
+    }
+  }
+
+  return { on, emit, off };
+}
+
 const emitter = new SimpleEventEmitter();
 
-emitter.on('greet', name => console.log(`Hello, ${name}!`));
-emitter.emit('greet', 'John'); // Hello, John!
-emitter.off('greet');
+emitter.on("greet", (name) => console.log(`Hello, ${name}!`));
+emitter.emit("greet", "John"); // Hello, John!
+emitter.off("greet");
